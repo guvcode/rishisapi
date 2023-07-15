d08263fdfac8f38e32c41e0f3f88e768dc220f6c
guvcode <gbengaomoni@gmail.com>
2023-07-15 10:05:25 -0600

    added console log

--- main.js
+++ main.js
@@ -1,51 +1,52 @@
 import Fastify from "fastify";
-import got from "got"; 
+import got from "got";
 import axios from "axios";
 
 const port = process.env.PORT || 3000;
 
 const fastify = Fastify({
-    logger: true,
-    requestTimeout: 30000, 
-})
+  logger: true,
+  requestTimeout: 30000,
+});
 
 fastify.listen({ port }, function (err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
   }
- 
+
   fastify.log.info(`Fastify is listening on port: ${address}`);
 });
 
-fastify.get('/', function (request, reply) {
-    reply.send ("Hello World")
-})
+fastify.get("/", function (request, reply) {
+  reply.send("Hello World");
+});
 
+fastify.get("/welcome", function (request, reply) {
+  reply.send("Welcome");
+});
 
-fastify.get('/welcome', function (request, reply) {
-    reply.send ("Welcome")
-})
-
-fastify.get('/ftoc/:temp', function (request,reply) {
-    const {temp} = request.params ;    
-    //console.log(temp)
-    ///console.log(user)
-    const tempincelcius = (temp - 32) /1.8
-    reply.send(tempincelcius);
-
-})
-fastify.get("/ctof/:temp", function (request, reply) {
-  console.log (request);
+fastify.get("/ftoc/:temp", function (request, reply) {
   const { temp } = request.params;
-  const tempinFahrenheit = (temp * 1.8) + 32;
+  //console.log(temp)
+  ///console.log(user)
+  const tempincelcius = (temp - 32) / 1.8;
+  reply.send(tempincelcius);
+});
+fastify.get("/ctof/:temp", function (request, reply) {
+  console.log(request);
+  const { temp } = request.params;
+  const tempinFahrenheit = temp * 1.8 + 32;
   reply.send(tempinFahrenheit);
-})
+});
 
-fastify.get("/weather/:city", async function (request, reply){
-  const {city} = request.params;
+fastify.get("/weather/:city", async function (request, reply) {
+  const { city } = request.params;
+  console.log(city);
   var apiToCall =
-    "https://api.openweathermap.org/data/2.5/weather?q=" & city & "&appid=d29300a88f0ef96ff3588b6c3e5ec09d";
+    "https://api.openweathermap.org/data/2.5/weather?q=" &
+    city &
+    "&appid=d29300a88f0ef96ff3588b6c3e5ec09d";
 
   axios
     .get(apiToCall)
@@ -60,4 +61,4 @@ fastify.get("/weather/:city", async function (request, reply){
     .finally(function () {
       // always executed
     });
-})
+});
